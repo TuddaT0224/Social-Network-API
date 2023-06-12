5cd0a8de506d3c4af14bb8e678ed0ae6ef251d18
Tudda <trezzt0224@gmail.com>
2023-06-12 14:23:38 -0500

    Updating User.js adding models

--- models/User.js
+++ models/User.js
@@ -1,1 +1,23 @@
 const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought');
+
+//Schema to create User model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trimmed: true
+
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            // Here we will use Regex to help validate email
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+        },
+        
+    }
+)
