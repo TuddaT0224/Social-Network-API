a2ee05418d3f92e53659f968932b7bd5a3718a09
Tudda <trezzt0224@gmail.com>
2023-06-12 14:40:24 -0500

    Updated Users.js

--- models/Users.js
+++ models/Users.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require('mongoose');
-const thoughtSchema = require('./Thought');
+const thoughtSchema = require('./Thoughts');
 
 //Schema to create User model
 const userSchema = new Schema(
@@ -18,6 +18,26 @@ const userSchema = new Schema(
             // Here we will use Regex to help validate email
             match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
         },
-        
-    }
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thoughts'
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Users'
+        }]
+    },
+    {toJSON: {
+        virtuals: true,
+    },
+    id: false
+}
 )
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
+
+const Users = model('Users', userSchema);
+
+module.exports = Users
