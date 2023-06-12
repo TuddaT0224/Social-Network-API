fff144b3153cfcd2f24bd33716a7dcc6bd78363d
Tudda <trezzt0224@gmail.com>
2023-06-12 17:37:17 -0500

    Updated userRoutes.js

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -12,3 +12,14 @@ const {
     deleteFriend
 } = require('../../controllers/userController');
 
+// Targets to `/api/users 
+router.route('/').get(getAllUsers).post(createUsers);
+// Targets to `/api/users/:userId
+router.route('/:userId').get(getSingleUser).put(updateUsers).delete(deleteUser);
+// Targets to `/api/users/:userId/friends
+router.route('/:userId/friends').post(addFriends);
+// Targets to `/api/users/:userId/friends/:friendsId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend)
+
+// Module export router
+module.exports = router;
