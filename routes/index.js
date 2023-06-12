a5f5a1995b67119bb43a925637cc42e65862de08
Tudda <trezzt0224@gmail.com>
2023-06-12 16:14:49 -0500

    Added index.js to routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
+// Require express router
 const router = require('express').Router();
+// import all of the api routes
 const apiRoutes = require('./api');
-
+//add `/api` to all of the api routes
 router.use('/api', apiRoutes);
-
-router.use((req, res) => res.send('Wrong route!'));
-
+//Status error message
+router.use((req, res) => res.send('Error!'));
+// Module exports router
 module.exports = router;
