a183657e23f1341879af9bdf267e105d4a00440a
Tudda <trezzt0224@gmail.com>
2023-06-13 23:15:14 -0500

    Continue to work on userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,42 @@ module.exports = {
         Users.create(body)
         .then(dbUsersData => res.json(dbUsersData))
         .catch(err => res.status(400).json(err));
-    }
+    },
+    //Get All Users
+    getAllUsers(req, res) {
+        Users.find({})
+        //populate users thoughts
+        .populate({path: 'thoughts', select: '-__v'})
+        // populate user friends
+        .populate({path: 'friends', select: '-__v'})
+        .select('-__v')
+        // sort Id
+        .then(dbUsersData => res.json(dbUsersData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+    // Get single user by Id
+    getUsersById({params}, res) {
+        Users.findOne({ _id: params.id })
+        .populate({path: 'thoughts', select: '-__v'})
+        .populate({path: 'friends', select: '-__v'})
+        .select('-__v')
+        // return if no user is found
+        .then(dbUsersData => {
+            if(!dbUsersData) {
+                res.status(404).json({message: 'No User found to match this Id!'});
+                return;
+            }
+            res.json(dbUsersData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err)
+        })
+    },
+    // Update a current User by Id
 }
 
 
