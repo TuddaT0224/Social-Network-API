e978c4490486f41da3565486b92229dcc05abf38
Tudda <trezzt0224@gmail.com>
2023-06-14 21:54:43 -0500

    added server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+// require express and mongoose
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+//connect mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+
+app.listen(PORT, () => {
+    console.log(`API server for ${activity} running on port ${PORT}!`);
+  });
