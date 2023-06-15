1ea3095701cb25f35a4062766a0c412c0b22da23
Tudda <trezzt0224@gmail.com>
2023-06-14 20:42:19 -0500

    Finished up the userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -44,6 +44,73 @@ module.exports = {
         })
     },
     // Update a current User by Id
-}
+    updateUsers({params, body}, res) {
+        Users.findOneAndUpdate({_id: params.id}, body, {runValidators: true, new: true})
+        .then(dbUsersData => {
+            if(!dbUsersData) {
+                res.status(404).json({ message: 'No User with this Id!'});
+                return;
+            }
+            res.json(dbUsersData);
+        })
+        .catch((err) => res.status(500).json(err));
+    },
+
+    // Delete a User by ID
+    deleteUsers({params}, res) {
+        Users.findOneAndDelete({_id: params.id})
+        .then(dbUsersData => {
+            if(!dbUsersData) {
+                res.status(404).json({ message: 'No such User exists'});
+                return;
+            }
+            res.json(dbUsersData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+    // Add a friend
+    addFriend({params}, res) {
+        Users.findOneAndUpdate({_id: params.id}, {$push: { friends: params.friendId}}, {new: true})
+        .populate({path: 'friends', select: ('-__v')})
+        .select('-__v')
+        .then(dbUsersData => {
+            if (!dbUsersData) {
+                res.status(404).json({message: 'No User with this Id!'});
+                return;
+            }
+            res.json(dbUsersData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },  
+    
+    // Delete a current friend
+    deleteFriend({ params }, res) {
+        Users.findOneAndRemove({_id: params.id}, {$pull: { friends: params.friendId}}, {new: true})
+        .populate({path: 'friends', select: '-__v'})
+        .select('-__v')
+        .then(dbUsersData => {
+            if(!dbUsersData) {
+                res.status(404).json({ message: 'No User with this Id!'});
+                return;
+            }
+            res.json(dbUsersData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    }
+        
+    
+};
+
+// Export modul users controller
+module.exports = userController;
 
 
