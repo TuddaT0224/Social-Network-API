cadb83f0eaa8ab90fa410f4485d272ed79176bca
Tudda <trezzt0224@gmail.com>
2023-06-14 21:10:42 -0500

    Added thougthts by Id to thought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -33,4 +33,23 @@ module.exports = {
             res.status(500).json(err);
         });
     },
-}
+
+    // Get thoughts by ID
+    getThoughtsById({params}, res) {
+        Thoughts.findOne({ _id: params.id })
+        .populate({path: 'reactions', select: '-__v'})
+        .select('-__v')
+        .then(dbThoughtsData => {
+            if(!dbThoughtsData) {
+                res.status(404).json({ message: 'No Thoughts linked to this ID!'});
+                return;
+            }
+            res.json(dbThoughtsData)
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
+    
